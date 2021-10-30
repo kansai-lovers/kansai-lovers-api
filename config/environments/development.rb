013627feebc437c0e0723ce253d2ca35466e92db
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-10-30 23:09:59 +0900

    fix host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,8 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts << "rocky-eyrie-43090.herokuapp.com"
-  
+  config.hosts << "kansai-lovers-api.herokuapp.com"
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
