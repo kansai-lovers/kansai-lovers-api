e8f228f4d54e4c7bdac7044a2f02ff77fd189090
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-11-07 01:07:24 +0900

    FIX: member一蘭を取得するAPI

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -2,6 +2,10 @@ module Api
   module V1
     class MembersController < ApplicationController
       def index
+        render json: Member.all
+      end
+
+      def show
         member = Member.find_by(id: params[:members_id])
         render json: member
       end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      get '/members/:members_id', to: 'members#index'
+      get '/members', to: 'members#index'
+      get '/members/:members_id', to: 'members#show'
       get '/members/:members_id/messages', to: 'messages#index'
     end
   end
