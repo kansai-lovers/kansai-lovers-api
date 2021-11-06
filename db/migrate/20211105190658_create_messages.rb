d97a30cb42b3dd6386354ea56da62030559d3f3b
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-11-06 16:14:35 +0900

    ADD: データベースの初期化 (#5)
    * ADD: データベースの初期化
    
    * FIX: 不要なファイル消したり

---
+++ db/migrate/20211105190658_create_messages.rb
@@ -0,0 +1,20 @@
+class CreateMessages < ActiveRecord::Migration[6.1]
+  def change
+    create_table :messages do |t|
+      t.references :members, null: false, foreign_key: true
+      t.references :repositories, null: false, foreign_key: true
+      t.text :text , null: false
+      t.text :url , null: false
+      t.datetime :join_day , null: false
+      t.integer :good_count , null: false, default: 0
+      t.integer :bad_count , null: false, default: 0
+      t.integer :laugh_count , null: false, default: 0
+      t.integer :hooray_count , null: false, default: 0
+      t.integer :confuse_count , null: false, default: 0
+      t.integer :heart_count , null: false, default: 0
+      t.integer :rocket_count , null: false, default: 0
+      t.integer :eyes_count , null: false, default: 0
+      t.timestamps
+    end
+  end
+end
