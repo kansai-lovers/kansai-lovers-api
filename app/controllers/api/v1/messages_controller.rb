1d3cbe5d7aef99484657e28f411e34a76782c220
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-11-07 00:58:04 +0900

    format

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,10 +2,11 @@ module Api
   module V1
     class MessagesController < ApplicationController
       def index
-        top_communication_message = Message.where(members_id: params[:members_id]).order(heart_count: "DESC").first
-        top_implementation_message = Message.where(members_id: params[:members_id]).order(rocket_count: "DESC").first
-        top_communication_message = Message.where(members_id: params[:members_id]).order(hooray_count: "DESC").first
-        top_messages = {top_communication_message: top_communication_message, top_implementation_message: top_implementation_message, top_communication_message: top_communication_message}
+        top_communication_message = Message.where(members_id: params[:members_id]).order(heart_count: 'DESC').first
+        top_implementation_message = Message.where(members_id: params[:members_id]).order(rocket_count: 'DESC').first
+        top_design_message = Message.where(members_id: params[:members_id]).order(hooray_count: 'DESC').first
+        top_messages = { top_communication_message: top_communication_message, top_implementation_message: top_implementation_message,
+          top_design_message: top_design_message }
         render json: top_messages
       end
     end
