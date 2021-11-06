f7993c3dca36ee98c0be371a868e015f67ace91b
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-11-07 02:58:52 +0900

    ゴリにゃ『綜志、はよAPI作ってや』 (#8)
    * ADD: messagesを取得するAPIを定義
    
    * format
    
    * format
    
    * FIX: member一蘭を取得するAPI
    
    * ADD: 偏差値の実装追加
    
    * format

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -2,8 +2,18 @@ module Api
   module V1
     class MembersController < ApplicationController
       def index
-        @member = Member.find_by(id: params[:members_id])
-        render json: @member
+        render json: Member.all
+      end
+
+      def show
+        member = Member.find_by(id: params[:members_id])
+        render json: member
+      end
+
+      def calcurate_score
+        member = Member.find_by(id: params[:members_id])
+        skill_score = { "design": member.dev_design, "implementation": member.dev_implementation, "communication": member.dev_communication }
+        render json: skill_score
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      get '/members/:members_id', to: 'members#index'
+      get '/members', to: 'members#index'
+      get '/members/:members_id', to: 'members#show'
+      get '/members/:members_id/skillScore', to: 'members#calcurate_score'
+      get '/members/:members_id/messages', to: 'messages#index'
     end
   end
 end
