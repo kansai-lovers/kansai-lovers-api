04a2a1728b9117074f2cb28752afad33ca756ed5
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-11-07 03:02:48 +0900

    FIX

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace 'v1' do
       get '/members', to: 'members#index'
       get '/members/:members_id', to: 'members#show'
-      get '/members/:members_id/skillScore', to: 'members#calcurate_score'
+      get '/members/:members_id/skillScores', to: 'members#calcurate_score'
       get '/members/:members_id/messages', to: 'messages#index'
     end
   end
