10119c9ba5dc07a1aedb4a3c0ce5504901c40631
Soshi Naito <44168191+SoshiNaito@users.noreply.github.com>
2021-11-07 13:48:52 +0900

    cors

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,15 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # Permit cross origin
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*",
+          headers: :any,
+          methods: [:get, :post, :options, :head]
+      end
+    end
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
